2a957b08fc0c3508bf26b0465a2ce25498e61236
trel1 <trel@netcraft.global>
2020-08-17 17:00:20 +0300

    todo redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import {render} from 'react-dom';
+import { createStore, compose, applyMiddleware } from 'redux';
+import {Provider} from 'react-redux'
+import thunk from 'redux-thunk'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { rootReducer } from './redux/rootReducer';
 
-ReactDOM.render(
-  <React.StrictMode>
+const store = createStore(rootReducer, compose(
+  applyMiddleware(
+    thunk
+  ),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+))
+
+const app = (
+  <Provider store={store}>
     <App />
+  </Provider>
+)
+
+render(
+  <React.StrictMode>
+    {app}
   </React.StrictMode>,
   document.getElementById('root')
 );
